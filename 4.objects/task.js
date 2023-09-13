5c7002f8be4a5dc6ffe9e6222437d217d1e35b27
BerryDream7 <alina-kud@mail.ru>
2023-09-14 00:42:08 +0300

    Объекты задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -4,43 +4,44 @@ function Student(name, gender, age) {
   this.age = age;
   this.marks = [];
 
-  this.setSubject = function(subjectName) {
-    this.subject = subjectName;
-  };
+}
 
-  this.addMarks = function(...marksToAdd) {
-    if (!this.marks) {
-        console.log("Ошибка: Студент был отчислен.");
-        return;
-    }
-    this.marks.push(...marksToAdd);
-  };
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
 
-  this.getAverage = function() {
-    if (!this.marks || !this.marks.length) {
-        return 0;
-    }
-    const sum = this.marks.reduce((acc, curr) => acc + curr);
-    return sum / this.marks.length;
-  };
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (!this.marks) {
+    console.log("Error: Student has been expelled.");
+    return;
+  }
+  this.marks.push(...marksToAdd);
+}
 
-  this.exclude = function(reason) {
-    delete this.subject;
+Student.prototype.getAverage = function () {
+  if (!this.marks || !this.marks.length) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, curr) => acc + curr);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
     delete this.marks;
     this.excluded = reason;
-  };
 }
 
 
-let student1 = new Student('Василиса', 'женский', 19);
-student1.setSubject('Algebra');
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
 console.log(student1.getAverage()); // 0
 student1.addMarks(4, 5, 4, 5);
 console.log(student1.getAverage()); // 4.5
 console.log(student1);
 // {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
 let student2 = new Student("Артём", "мужской", 25);
-student2.setSubject('Geometry');
-student2.exclude('плохая учёба')
+student2.setSubject("Geometry");
+student2.exclude("плохая учёба")
 console.log(student2)
 // {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
