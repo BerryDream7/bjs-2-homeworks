3c0fd7780232994973f7146a047257f3c5c23418
BerryDream7 <alina-kud@mail.ru>
2023-09-23 00:45:46 +0300

    Задание выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿// Задача 1
+
+function parseCount(a) {
+    let number = Number.parseFloat(a);
+    if (Number.isNaN(Number.parseFloat(a))){
+        throw new Error("Невалидное значение");
+    }
+    return number;
+}
+
+function validateCount(a) {
+    try{
+        return parseCount(a);
+    } catch (error){
+        return error;
+    }
+}
+
+// Задача 2
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (((a + b) < c) || ((b + c) < a) || ((a + c) < b)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.getPerimeter / 2;
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try{
+        return new Triangle(a, b, c);
+    } catch (error){
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
